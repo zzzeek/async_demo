4c63c0bcb21889ef24ece04a12768f14d4496310
Mike Bayer <mike_mp@zzzcomputing.com>
2024-05-18 00:00:23 -0400

    dev

--- samples/nonblocking_io_w_greenlet.py
+++ samples/nonblocking_io_w_greenlet.py
@@ -1,100 +1,54 @@
-# this doesnt work yet but it will, and it's going to be cool
-
-import socket
 import greenlet
 import select
+import socket
 
-thingies = []
-thingies_by_sock = {}
+class event_loop(greenlet.greenlet):
+    def run(self, socket_comms):
+        comms_by_sock = {t.sock: t for t in socket_comms}
 
+        while comms_by_sock:
+            t_list = {
+                "notstarted": (t_start := []), "reader": (t_reader := []),
+                "writer": (t_writer := []), "done": (t_done := [])
+            }
+            for sock, t in comms_by_sock.items():
+                t_list[t.mode].append(sock)
 
-class loop(greenlet.greenlet):
-    def run(self):
-        while True:
-            print("LOOP 1")
-            for thingy in thingies:
-                if thingy.mode == "none" and not thingy.dead:
-                    thingy.switch()
+            if t_reader or t_writer:
+                t_reader, t_writer, _ = select.select(t_reader, t_writer, [])
 
-            print("LOOP 2")
-            readers, writers, _ = select.select(
-                [
-                    sock
-                    for sock, t in thingies_by_sock.items()
-                    if t.mode == "reader"
-                ],
-                [
-                    sock
-                    for sock, t in thingies_by_sock.items()
-                    if t.mode == "writer"
-                ],
-                [],
-            )
-            print("LOOP 3")
-            for writer in writers:
-                thingies_by_sock[writer].switch()
+            for sock in t_start + t_reader + t_writer:
+                comms_by_sock[sock].switch(self)
+            for sock in t_done:
+                del comms_by_sock[sock]
 
-            print("LOOP 4")
-            for reader in readers:
-                thingies_by_sock[reader].switch()
-
-
-class thingy(greenlet.greenlet):
-    mode = "none"
-
+class socket_comm(greenlet.greenlet):
     def __init__(self, msg):
+        self.mode = "notstarted"
         self.msg = msg
-        self.result = ""
-        thingies.append(self)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def run(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(("tcpbin.com", 4242))
-        sock.setblocking(False)
+    def run(self, loop):
+        self.sock.connect(("tcpbin.com", 4242))
+        self.sock.setblocking(False)
 
-        print("step1")
-        self.send_writer(sock, f"message number {self.msg}\n".encode("ascii"))
-        print("step2")
-        self.receive_reader(sock)
-        event_loop.switch()
-
-    def send_writer(self, sock, msg):
         self.mode = "writer"
-        thingies_by_sock[sock] = self
-        event_loop.switch()
+        tosend = f"message number {self.msg}\n".encode("ascii")
+        while tosend:
+            loop.switch()
+            sent = self.sock.send(tosend)
+            tosend = tosend[sent:]
 
-        while True:
-            sent = sock.send(msg)
-            self.msg = self.msg[sent:]
-            if not self.msg:
-                self.mode = "none"
-                del thingies_by_sock[sock]
-                break
-            else:
-                event_loop.switch()
+        self.mode = "reader"; data = ""; result = ""
+        while not result or (data and len(data) >= 1024):
+            loop.switch()
+            data = self.sock.recv(1024)
+            result += data.decode("utf-8")
 
-    def receive_reader(self, sock):
-        print(f"RECV FOR  {self.msg}")
-        self.mode = "reader"
-        thingies_by_sock[sock] = self
-        event_loop.switch()
+        print(f"Got back echo response: {result}")
+        self.mode = "done"
+        loop.switch()
 
-        while True:
-            data = sock.recv(1024)
-            if data:
-                self.result += data.decode("utf-8")
-
-            if not data or len(data) < 1024:
-                print(f"Got back echo response: {self.result}")
-                self.mode = "none"
-                del thingies_by_sock[sock]
-                break
-            else:
-                event_loop.switch()
-
-
-event_loop = loop()
-
-for msg in ["one", "two", "three", "four", "five"]:
-    thingy(msg)
-event_loop.switch()
+event_loop().switch(
+    [socket_comm(msg) for msg in ["one", "two", "three", "four", "five"]]
+)
